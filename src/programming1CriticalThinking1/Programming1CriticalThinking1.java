89ab75aea645b5770ddde963611c28c3f5bdacb5
Shayna A. <shaynaalmond@gmail.com>
2023-08-21 11:40:55 -0500

    What should a commit message say?

---
+++ src/programming1CriticalThinking1/Programming1CriticalThinking1.java
@@ -0,0 +1,26 @@
+package programming1CriticalThinking1;
+
+/**
+ * This is a program to print the following information about
+ * a fictional person: First Name, Last Name, Street Address,
+ * City, and Zip Code.
+ * 
+ * @author Shayna Almond
+ *
+ */
+
+public class Programming1CriticalThinking1 {
+	/*
+	 * Create the values to be printed out
+	 * Print out the information for the user
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName = "Ariel";
+		String lastName = "Johnson";
+
+		System.out.println(firstName);
+	}
+
+}
