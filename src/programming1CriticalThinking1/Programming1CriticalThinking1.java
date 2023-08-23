fe6367c0649b1b834a8a137e29ec154be7a34e7f
Morriganmari <142628928+Morriganmari@users.noreply.github.com>
2023-08-23 11:40:04 -0500

    Write remaining program tested and finished

--- src/programming1CriticalThinking1/Programming1CriticalThinking1.java
+++ src/programming1CriticalThinking1/Programming1CriticalThinking1.java
@@ -6,21 +6,29 @@ package programming1CriticalThinking1;
  * City, and Zip Code.
  * 
  * @author Shayna Almond
- *
  */
 
 public class Programming1CriticalThinking1 {
 	/*
-	 * Create the values to be printed out
+	 * Create and initialize the values to be printed out
 	 * Print out the information for the user
 	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Initialize the variables to be printed out
 		String firstName = "Ariel";
 		String lastName = "Johnson";
+		String streetAddress = "418 King Blvd";
+		String city = "Houston";
+		int zipCode = 65809;
 
-		System.out.println(firstName);
+		//Print each variable with the name and variable to clarify for the user
+		System.out.println("First name: " + firstName);
+		System.out.println("Last name: " + lastName);
+		System.out.println("Street address: " + streetAddress);
+		System.out.println("City: " + city);  
+		System.out.println("Zipcode: " + zipCode);
 	}
 
 }
+ 
